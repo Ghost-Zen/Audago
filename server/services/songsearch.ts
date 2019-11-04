16f5f6c5e8ca8d43df6ab7c456672d9f3a41963b
Daniel <danielminter13@gmail.com>
2019-11-04 12:00:11 +0200

    made artwork image higher quality

--- server/services/songsearch.ts
+++ server/services/songsearch.ts
@@ -9,12 +9,17 @@ export default class SearchSong {
           .then(function (response) {
           let data = response.data.results
           for(let item of data){
+            let artwork = item.artworkUrl100
+            artwork = artwork.split('/')
+            let lastItemArray = artwork.length - 1;
+            artwork[lastItemArray] = '200x200bb.jpg'; //make image quality better.
+            artwork = artwork.join('/'); 
             let search = {
               artist: item.artistName,
               track: item.trackCensoredName,
               song: item.previewUrl,
               album: item.collectionCensoredName,
-              artwork: item.artworkUrl100
+              artwork
             }
               allSongsBySearch.push(search)
           }
