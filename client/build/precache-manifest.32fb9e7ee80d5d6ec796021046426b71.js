a0d60768be9a08c665797c843b0dc47ff8705327
daniel_8 <danielminter13@gmail.com>
2022-03-10 15:42:18 +0200

    React build update

---
+++ client/build/precache-manifest.32fb9e7ee80d5d6ec796021046426b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbb6db78c92994a0485f90d91b930d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93eeab9559abf857021b",
+    "url": "/static/css/2.b517d6a8.chunk.css"
+  },
+  {
+    "revision": "6c6288a10a408ddf8a2a",
+    "url": "/static/css/main.3f6cb202.chunk.css"
+  },
+  {
+    "revision": "93eeab9559abf857021b",
+    "url": "/static/js/2.34e584dc.chunk.js"
+  },
+  {
+    "revision": "6c6288a10a408ddf8a2a",
+    "url": "/static/js/main.012b488b.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
