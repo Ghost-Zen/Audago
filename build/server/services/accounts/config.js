cffcef475468e195a7e82969cf46986d961ac6d0
daniel_8 <danielminter13@gmail.com>
2022-03-10 15:34:33 +0200

    Remove buld folder

--- build/server/services/accounts/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Config;
-(function (Config) {
-    Config["SECRET"] = "secret_sauce";
-})(Config = exports.Config || (exports.Config = {}));
-//# sourceMappingURL=config.js.map
